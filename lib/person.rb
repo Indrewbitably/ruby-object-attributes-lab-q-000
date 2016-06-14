5d768a773e1dbbe4ac69dc5b9871869ae281b18b
Andrew Magalski <drew.magalski@gmail.com>
2016-06-14 18:56:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def job=(person_job)
+    @job = person_job
+  end
+  def job
+    @job
+  end
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+end
